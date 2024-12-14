2a9efe8f02b44c3d4aa5347be7689c67bf52effa
billa <billa2013@163.com>
2024-12-15 02:31:04 +0800

    config: 添加images配置

--- next.config.js
+++ next.config.js
@@ -3,6 +3,17 @@ const nextConfig = {
   experimental: {
     esmExternals: "loose",
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'rlscyjecgizuupwobasc.supabase.co',
+        port: '',
+        pathname: '/storage/v1/object/public/task_images/**',
+        search: '',
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
