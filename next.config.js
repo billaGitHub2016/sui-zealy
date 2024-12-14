76f356ca071d7020aaaa336383774dc9c1af81d0
billa <billa2013@163.com>
2024-12-15 02:48:09 +0800

    config: config修改

--- next.config.js
+++ next.config.js
@@ -4,13 +4,20 @@ const nextConfig = {
     esmExternals: "loose",
   },
   images: {
+    // remotePatterns: [
+    //   {
+    //     protocol: 'https',
+    //     hostname: 'rlscyjecgizuupwobasc.supabase.co',
+    //     port: '',
+    //     pathname: '/storage/v1/object/public/task_images/**',
+    //     search: '',
+    //   },
+    // ],
+    domains: ["rlscyjecgizuupwobasc.supabase.co"],
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'rlscyjecgizuupwobasc.supabase.co',
-        port: '',
-        pathname: '/storage/v1/object/public/task_images/**',
-        search: '',
+        protocol: "https",
+        hostname: "rlscyjecgizuupwobasc.supabase.co",
       },
     ],
   },
